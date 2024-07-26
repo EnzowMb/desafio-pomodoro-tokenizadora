7bf0a9eef4a7b204b9c763d08b915d4f1dc764a6
EnzoMbz <89809584+EnzowMb@users.noreply.github.com>
2024-07-26 20:30:23 -0300

    feat: add prisma database in nest and migration table task

--- back-end-nestjs/src/app.module.ts
+++ back-end-nestjs/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { PrismaService } from './database/prisma.service';
 
 @Module({
   imports: [TasksModule],
+  providers: [PrismaService],
 })
 export class AppModule {}
