d44648c0390d3b251cfde3f689325e13bfbbc0e9
EnzoMbz <89809584+EnzowMb@users.noreply.github.com>
2024-07-26 20:29:25 -0300

    fix: docker build and up

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,9 +1,9 @@
-CREATE USER enzo SUPERUSER PASSWORD 'des@fi0';
-CREATE DATABASE "taskPomodoro"
+CREATE USER enzo SUPERUSER PASSWORD 'desafio';
+CREATE DATABASE "taskpomodoro"
   WITH OWNER = postgres
        ENCODING = 'UTF8'
        TABLESPACE = pg_default
        LC_COLLATE = 'en_US.utf8'
        LC_CTYPE = 'en_US.utf8'
        CONNECTION LIMIT = -1;
-GRANT ALL PRIVILEGES ON DATABASE taskPomodoro TO enzo;
+GRANT ALL PRIVILEGES ON DATABASE taskpomodoro TO enzo;
