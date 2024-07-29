a4a278e1dc3b39e95be98754fe38ca696ee11a38
EnzoMbz <89809584+EnzowMb@users.noreply.github.com>
2024-07-29 17:32:08 -0300

    refactor: add fastify in main.ts and delete unused file entity

--- back-end-nestjs/src/main.ts
+++ back-end-nestjs/src/main.ts
@@ -1,15 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    }),
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
   );
   await app.listen(3000);
 }
--- back-end-nestjs/src/tasks/task.entity.ts
+++
@@ -1,8 +0,0 @@
-export class Task {
-  id: number;
-  title: string;
-  description: string;
-  completed: boolean;
-  createdAt: Date;
-  pomodoroCount: number;
-}
