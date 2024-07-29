532ed8df9655e33415fbee2f982654cd0233e6b2
EnzoMbz <89809584+EnzowMb@users.noreply.github.com>
2024-07-29 17:32:38 -0300

    refactor: add filter exception

--- back-end-nestjs/src/app.module.ts
+++ back-end-nestjs/src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { FilterExceptionGlobal } from './filters/filter-exception-http-global';
+import { APP_FILTER } from '@nestjs/core';
 
 @Module({
   imports: [TasksModule],
-  providers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: FilterExceptionGlobal,
+    },
+  ],
 })
 export class AppModule {}
